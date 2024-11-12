6701649fd351e709a155f94904dbcd4bbd7c0300
Raphael De Lio <raphadlts@gmail.com>
2024-11-12 12:42:40 +0100

    Implementing Sliding Window Log with Strings instead of a Sorted Set

--- src/main/kotlin/org/example/SlidingWindowLogRateLimiter.kt
+++ src/main/kotlin/org/example/SlidingWindowLogRateLimiter.kt
@@ -49,4 +49,17 @@ class SlidingWindowLogRateLimiter(
         }
         return isAllowed
     }
+
+    fun isAllowedStringAlternative(clientId: String): Boolean {
+        val requestKeyPattern = "rate_limit:$clientId:*"
+        val requestCount = jedis.keys(requestKeyPattern).size
+        val isAllowed = requestCount < limit
+
+        if (isAllowed) {
+            val uniqueKey = "rate_limit:$clientId:${UUID.randomUUID()}"
+            jedis.setex(uniqueKey, windowSize, "")
+        }
+
+        return isAllowed
+    }
 }
